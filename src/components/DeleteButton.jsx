5c0f0378f5430d1b6c320a5660dd0786a55b1a0a
Rodhis <rodrigonoliva@gmail.com>
2024-07-23 11:32:52 -0300

    added a redirect to delete button to avoid undefined errors

--- src/components/DeleteButton.jsx
+++ src/components/DeleteButton.jsx
@@ -1,5 +1,6 @@
 import PropTypes from "prop-types"
 import useStock from "../hooks/useStock"
+import { useNavigate } from "react-router-dom"
 
 DeleteButton.propTypes = {
     itemId: PropTypes.number,
@@ -8,10 +9,12 @@ DeleteButton.propTypes = {
 
 export default function DeleteButton({ itemId, itemName }) {
     const { deleteItem } = useStock()
+    const navigate = useNavigate()
 
     const handleDelete = () => {
         if (confirm(`Tem certeza que deseja excluir ${itemName}?`)) {
             deleteItem(itemId)
+            navigate("/items")
         }
     }
     return (
